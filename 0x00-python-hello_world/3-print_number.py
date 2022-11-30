26360d43c91fd298216c9361159675922566b8fa
fredkd <kimanifredrick807@gmail.com>
2022-11-29 22:51:41 -0800

    print number

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,393 @@
+#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+
+number = 98
+
+print("{:d} Battery street".format(number))
